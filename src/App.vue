bc0445dde5e715de397644c594fac82ae77bfe52
Austin Caron <austin.caron1@gmail.com>
2021-10-07 04:27:20 -0400

    added hero section, added swiper for sliding images, added placeholder images

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import Navigation from "./components/Navigation.vue";
 import Hero from "./components/Hero.vue";
+import Products from "./components/Products.vue";
 </script>
 
 <template>
   <Navigation />
   <Hero id="hero" class="hero"/>
+  <Products id="products" class="products"/>
 </template>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,13 +1,59 @@
 <template>
   <section>
-      
+      <swiper
+        :modules="modules"
+        :slides-per-view="1"
+        :space-between="0"
+        :pagination="{ clickable: true }"
+        :loop="true"
+        class="hero__wrapper"
+      >
+        <swiper-slide 
+          v-for="slide in sliderData" 
+          :key="slide.id" 
+          :style="{ background: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('/imgs/${slide.img}')`}"
+          class="hero__slide"
+        >
+        <h2 class="hero__heading">{{ slide.heading }}</h2>
+        </swiper-slide>
+      </swiper>
   </section>
 </template>
 
 <script>
-export default {
+ import { Pagination, A11y } from 'swiper';
 
-}
+  // Import Swiper Vue.js components
+  import { Swiper, SwiperSlide } from 'swiper/vue';
+
+  // Import Swiper styles
+  import 'swiper/css';
+  import 'swiper/css/pagination';
+
+  // Import Swiper styles
+  export default {
+    data() {
+      return {
+        sliderData: [
+          {id: 0, heading: "Feel the Difference", img: 'hero_placeholder.jpg'},
+          {id: 1, heading: "slide 2", img: 'hero_placeholder.jpg'},
+          {id: 2, heading: "slide 3", img: 'hero_placeholder.jpg'},
+          {id: 3, heading: "slide 4", img: 'hero_placeholder.jpg'},
+        ],
+      }
+    },
+
+    setup() {
+      return {
+        modules: [Pagination, A11y],
+      };
+    },
+
+    components: {
+      Swiper,
+      SwiperSlide,
+    },
+  };
 </script>
 
 <style>
