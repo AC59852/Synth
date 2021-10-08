1344afe88997324ad8853709b82fa6838c094f48
Austin Caron <austin.caron1@gmail.com>
2021-10-08 16:49:22 -0400

    added styling for mobile about section

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,13 +6,13 @@
             <img v-for="img in aboutImgs.slice(0,1)" :src="'/imgs/' + img.link" alt="Small Product Image" :key="img.id">
         </div>
         <div class="about__story">
-            <h2>My Story</h2>
+            <h2 class="about__title">My Story</h2>
             <div class="about__paraWrapper">
                 <p>{{ paraContent.para1 }}</p>
                 <p v-if="desktop || storyExpanded">{{ paraContent.para2 }}</p>
                 <p v-if="desktop || storyExpanded">{{ paraContent.para3 }}</p>
             </div>
-            <button @click="setStory()">{{ aboutBtn }}</button>
+            <button @click="setStory()" class="about__btn">{{ aboutBtn }}</button>
         </div>
         <div class="about__imgBox" v-if="desktop">
             <img v-for="img in aboutImgs.slice(2,3)" :src="'/imgs/' + img.link" alt="Small Product Image" :key="img.id">
@@ -32,7 +32,7 @@ export default {
 
             aboutBtn: 'Read More',
 
-            quote: `“I Couldn’t Find What I Needed, So I Made it Myself.”`,
+            quote: `I Couldn’t Find What I Needed, So I Made it Myself.`,
 
             paraContent: {
                 para1: `Orci orci, sed tellus et, malesuada lacus cursus et. Suscipit quis sit felis accumsan pellentesque ullamcorper. Enim justo, elementum duis consectetur id enim non sodales. Dui, ac nulla a, ut fermentum nunc libero, faucibus cras. Nulla morbi ipsum, tincidunt commodo enim fusce feugiat sagittis imperdiet. Blandit dictum pharetra ac diam ornare id. Proin id augue nunc fermentum eget sem curabitur interdum gravida. Purus dictum diam, tellus luctus ultrices auctor feugiat in cursus. Non a, aenean eu sed quam phasellus ullamcorper. Velit amet integer metus vestibulum turpis dui lectus. Morbi quis velit felis mattis congue. Ligula ut et felis vitae adipiscing feugiat risus turpis malesuada. Augue phasellus enim sit vel odio amet. Augue amet pulvinar purus vitae, a, amet viverra adipiscing suspendisse. Dictumst sed in donec elit purus ut tincidunt quisque.`,
